fc98667b2ac97e6e96ecbe3dac45465be0a74b46
yuki <meipapa0415@gmail.com>
2024-03-16 17:08:16 +0900

    2回目

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    // jQuery(function(){
+    //     const swiper__top= new Swiper('.js-top-swiper', {
+    //       effect: 'fade',
+    //       direction: 'horizontal',
+    //       loop: true,
+    //       autoplay: {
+    //         delay: 3000, // ミリ秒単位で自動再生の間隔を指定
+    //       },
+    //     });
+    //   });
 
 });
